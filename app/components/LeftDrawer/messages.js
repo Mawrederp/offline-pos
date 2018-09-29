65c37a63ac20f05f9dd1dde00524368ebf3754e0
akramabdulrahman <badassfart91>
2018-09-29 05:08:32 +0300

    =-[x] translations -[x] rtl fixes -[x] code validation -[x] optimizations for number of renders

---
+++ app/components/LeftDrawer/messages.js
@@ -0,0 +1,105 @@
+/*
+ * MultiVariantForm Messages
+ *
+ * This contains all the text for the MultiVariantForm component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  product: {
+    id: 'app.components.product',
+    defaultMessage: 'المنتج',
+  },
+  settings: {
+    id: 'app.components.settings',
+    defaultMessage: 'اعدادات',
+  },
+  variform: {
+    id: 'app.components.variform',
+    defaultMessage: 'متعدد الاشكال',
+  },
+  EmployeeName: {
+    id: 'app.components.EmployeeName',
+    defaultMessage: 'اسم الموظف',
+  },
+  time: {
+    id: 'app.components.time',
+    defaultMessage: 'الوقت',
+  },
+  name: {
+    id: 'app.components.name',
+    defaultMessage: 'اسم',
+  },
+  quantity: {
+    id: 'app.components.quantity',
+    defaultMessage: 'الكمية',
+  },
+  thePrice: {
+    id: 'app.components.thePrice',
+    defaultMessage: 'السعر',
+  },
+  price: {
+    id: 'app.components.price',
+    defaultMessage: 'سعر',
+  },
+  barcode: {
+    id: 'app.components.barcode',
+    defaultMessage: 'الباركود',
+  },
+  criteria: {
+    id: 'app.components.criteria',
+    defaultMessage: 'الوصف',
+  },
+  discount: {
+    id: 'app.components.discount',
+    defaultMessage: 'الخصم',
+  },
+  quantityUnit: {
+    id: 'app.components.quantityUnit',
+    defaultMessage: 'وحدة',
+  },
+  basic: {
+    id: 'app.components.basic',
+    defaultMessage: 'اساسي',
+  },
+  feature: {
+    id: 'app.components.feature',
+    defaultMessage: 'السمة',
+  },
+  addNewFeature: {
+    id: 'app.components.addNewFeature',
+    defaultMessage: 'اضافة سمة جديدة',
+  },
+  deleteFeature: {
+    id: 'app.components.deleteFeature',
+    defaultMessage: 'حذف السمة',
+  },
+  overall: {
+    id: 'app.components.overall',
+    defaultMessage: '{action} الكلي ',
+  },
+  nameOf: {
+    id: 'app.components.nameOf',
+    defaultMessage: 'اسم {thing}',
+  },
+  custom: {
+    id: 'app.components.custom',
+    defaultMessage: '{action} مخصص ',
+  },
+  currencySrInitials: {
+    id: 'app.components.currencySrInitials',
+    defaultMessage: 'ريال',
+  },
+  productName: {
+    id: 'app.components.productName',
+    defaultMessage: '{name} {product}',
+  },
+  productVariformSettings: {
+    id: 'app.components.ProductsModal.ProductVariformSettings',
+    defaultMessage: '{settings} {product} {variform}',
+  },
+});
+/*,
+      nameOf,
+      addNewFeature,
+      deleteFeature,*/
